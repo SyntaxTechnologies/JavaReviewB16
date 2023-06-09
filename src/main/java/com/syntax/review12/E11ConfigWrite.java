6eb86ce1c2407e9e5542f2cb266a1c5fb7381f01
Asghar <instructor@syntaxtechs.com>
2023-06-08 22:02:15 -0400

    Review class 12

--- src/main/java/com/syntax/review12/E11ConfigWrite.java
+++ src/main/java/com/syntax/review12/E11ConfigWrite.java
@@ -1,4 +1,22 @@
 package com.syntax.review12;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class E11ConfigWrite {
+
+    public static void main(String[] args) throws IOException {
+        String path="Files/Test.properties";
+        FileInputStream fileInputStream=new FileInputStream(path);
+        Properties properties=new Properties();
+        properties.load(fileInputStream);
+        properties.setProperty("Name","Vlad");
+        FileOutputStream fos=new FileOutputStream(path);
+        properties.store(fos,"adding a name");
+
+    }
+
 }
