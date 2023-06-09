51ec829ebaf582135f9b463e8b48b1c9782127d9
Asghar <instructor@syntaxtechs.com>
2023-06-08 21:01:18 -0400

    Review class 12

---
+++ src/main/java/com/syntax/review12/Dog.java
@@ -0,0 +1,4 @@
+package com.syntax.review12;
+
+public class Dog {
+}
--- src/main/java/com/syntax/review12/E7InterviewQuestion.java
+++ src/main/java/com/syntax/review12/E7InterviewQuestion.java
@@ -2,6 +2,8 @@ package com.syntax.review12;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class E7InterviewQuestion {
     public static void main(String[] args) {
@@ -13,6 +15,25 @@ public class E7InterviewQuestion {
         40,1
         50,1
          */
-        int [] arr={10,20,10,30,40,50,20,30,70,80,70};
+        // break till 9:01
+        int[] arr = {10, 20, 10, 30, 40, 50, 20, 30, 70, 80, 70};
+
+
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int num : arr
+        ) {
+            if (map.get(num) == null) {
+                map.put(num,1);
+            }else
+            {
+                int count = map.get(num) + 1;
+                map.replace(num, count);
+            }
+
+        }
+        System.out.println(map);
+
+
     }
 }
