b0ef92def0f4ce197b22322cfcd834c7132a8bc8
Asghar <instructor@syntaxtechs.com>
2023-06-08 20:14:49 -0400

    Review class 12

--- src/main/java/com/syntax/review12/E7InterviewQuestion.java
+++ src/main/java/com/syntax/review12/E7InterviewQuestion.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
 public class E7InterviewQuestion {
     public static void main(String[] args) {
         // print the numbers with their count(how many times they appeared in the array)
+        /*
+        10,2
+        20,2
+        30,2
+        40,1
+        50,1
+         */
         int [] arr={10,20,10,30,40,50,20,30,70,80,70};
     }
 }
